765dad5fbf9985286af936edd526fc73a22dd20e
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-06-01 19:26:14 +0700

    Add feature: show datas

--- src/main/java/repository/TodoListRepositoryImpl.java
+++ src/main/java/repository/TodoListRepositoryImpl.java
@@ -3,10 +3,9 @@ package repository;
 import entity.Todolist;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoListRepositoryImpl implements TodoListRepository {
 
@@ -18,7 +17,26 @@ public class TodoListRepositoryImpl implements TodoListRepository {
 
   @Override
   public Todolist[] getAll() {
-    return data;
+    String sql = "SELECT * FROM todolist";
+
+    try (Connection connection = dataSource.getConnection();
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery(sql)) {
+
+      List<Todolist> list = new ArrayList<>();
+      while (resultSet.next()) {
+        Todolist todolist = new Todolist();
+        todolist.setId(resultSet.getInt("id"));
+        todolist.setTodo(resultSet.getString("todo"));
+
+        list.add(todolist);
+      }
+
+      return list.toArray(new Todolist[]{});
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 
@@ -36,14 +54,14 @@ public class TodoListRepositoryImpl implements TodoListRepository {
     }
   }
 
-  private boolean isExists(Integer number){
+  private boolean isExists(Integer number) {
     String sql = "SELECT * FROM todolist WHERE id = ?";
     try (Connection connection = dataSource.getConnection();
-    PreparedStatement statement = connection.prepareStatement(sql)) {
+         PreparedStatement statement = connection.prepareStatement(sql)) {
       statement.setInt(1, number);
 
-      try(ResultSet resultSet = statement.executeQuery()){
-        if (resultSet.next()){
+      try (ResultSet resultSet = statement.executeQuery()) {
+        if (resultSet.next()) {
           return true;
         } else {
           return false;
@@ -57,7 +75,7 @@ public class TodoListRepositoryImpl implements TodoListRepository {
 
   @Override
   public boolean remove(Integer number) {
-    if (isExists(number)){
+    if (isExists(number)) {
       String sql = "DELETE FROM todolist WHERE id = ?";
       // Try with resource (Biar auto close)
       try (Connection connection = dataSource.getConnection();

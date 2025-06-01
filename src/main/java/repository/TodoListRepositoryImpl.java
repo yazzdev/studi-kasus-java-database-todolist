c64af00c1097e141476cae9fa33ad8d13241d309
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2025-06-01 19:11:41 +0700

    Remove methode (TodoListRepositoryImpl): isFull(), resizeIfFull(), and array TodoList[]

--- src/main/java/repository/TodoListRepositoryImpl.java
+++ src/main/java/repository/TodoListRepositoryImpl.java
@@ -10,8 +10,6 @@ import java.sql.SQLException;
 
 public class TodoListRepositoryImpl implements TodoListRepository {
 
-  public Todolist[] data = new Todolist[10];
-
   private DataSource dataSource;
 
   public TodoListRepositoryImpl(DataSource dataSource) {
@@ -23,30 +21,6 @@ public class TodoListRepositoryImpl implements TodoListRepository {
     return data;
   }
 
-  public boolean isFull() {
-    // Cek apakah model penuh?
-    var isFull = true;
-    for (var i = 0; i < data.length; i++) {
-      if (data[i] == null) {
-        // Model masih ada yang kosong
-        isFull = false;
-        break;
-      }
-    }
-    return isFull;
-  }
-
-  public void resizeIfFull() {
-    // Jika penuh, resize ukuran array menjadi 2x lipat
-    if (isFull()) {
-      var temp = data;
-      data = new Todolist[data.length * 2];
-
-      for (var i = 0; i < temp.length; i++) {
-        data[i] = temp[i];
-      }
-    }
-  }
 
   @Override
   public void add(Todolist todolist) {
